a62c5ba2b59e913f9f1f314f219fde3333ab4671
Sheran <gt-sherantv@hotmail.com>
2022-02-03 07:27:00 -0500

    add read more function

---
+++ script.js
@@ -0,0 +1,31 @@
+function myFunction1() {
+    var dots = document.getElementById("p1-dots");
+    var moreText = document.getElementById("p1-more");
+    var btnText = document.getElementById("myBtn1");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
+
+function myFunction2() {
+    var dots = document.getElementById("p2-dots");
+    var moreText = document.getElementById("p2-more");
+    var btnText = document.getElementById("myBtn2");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
